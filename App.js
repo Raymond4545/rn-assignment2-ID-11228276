d5168d03ede72977338bf3813659d28aad531394
“Raymond <raymondakenten@icloud.com>
2024-05-27 11:36:37 -0400

    Changed Text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on yo </Text>
+      <Text>My name is Raymond</Text>
       <StatusBar style="auto" />
     </View>
   );
