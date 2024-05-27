fe9fe69e1af6c17e3b6073767b002abae9a15022
“Raymond <raymondakenten@icloud.com>
2024-05-27 11:39:25 -0400

    Changed Text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Raymond</Text>
+      <Text style={styles.text}>My name is Raymond</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:24,
+    color:'white'
+  }
 });
