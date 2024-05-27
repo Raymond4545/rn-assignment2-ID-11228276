fb75b2dde9146fa8c4cd49bc6490b49755a91124
“Raymond <raymondakenten@icloud.com>
2024-05-27 11:41:20 -0400

    Made name bold

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Raymond</Text>
+      <Text style={styles.text}>My name is <Text style={{fontWeight:'bold'}}>Raymond</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
